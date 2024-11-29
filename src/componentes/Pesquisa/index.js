322ebea6e34a088ef6f5c71b6f499ccb21a2a665
Sarah Kédma <sarahkedmacontact@gmail.com>
2024-11-29 11:38:12 -0300

    livros

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -25,6 +25,23 @@ const Subtitulo = styled.h3`
         font-weight: 500;
         margin-bottom: 40px;
 `
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    p {
+        width: 200px;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
+
 
 function Pesquisa () {
     const [livrosPesquisados, setLivrosPesquisados] = useState([])
@@ -42,7 +59,12 @@ function Pesquisa () {
                     setLivrosPesquisados(resultadoPesquisa)
             }}
             />
-
+    { livrosPesquisados.map( livro => (
+        <Resultado>
+        <p>{livro.nome}</p>
+        <img src={livro.src} alt={livro.nome}/>
+        </Resultado>
+    ))}
     </PesquisaContainer>
 
     )
