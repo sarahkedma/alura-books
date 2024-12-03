560c3fdbe6ef2a0718dbe196a4b54e9955e6fcd7
Sarah Kédma <sarahkedmacontact@gmail.com>
2024-12-03 01:35:39 -0300

    props card

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -3,6 +3,9 @@
 import { livros } from './dadosUltimosLancamentos'
 import styled from 'styled-components';
 import { Titulo } from '../Titulo';
+import CardRecomenda from '../CardRecomenda';
+import imagemLivro from '../../imagens/livro2.png'
+
 
 const UltimosLancamentosContainer = styled.section`
     background-color: #EBECEE;
@@ -22,19 +25,25 @@ const NovosLivrosContainer = styled.div`
 function UltimosLancamentos() {
     return (
         <UltimosLancamentosContainer>
-        <Titulo 
-        cor="#EB9B00"
-        tamanhoFonte="36px"
-        >
-        ÚLTIMOS LANÇAMENTOS 
-        </Titulo>
-        < NovosLivrosContainer >
-        {livros.map ( livro => (
-            <img src={livro.src} alt={livro.nome}/>
-        ))}  
-        </NovosLivrosContainer>
+            <Titulo
+                cor={"#EB9B00"}
+                tamanhoFonte={"36px"}
+            >
+                ÚLTIMOS LANÇAMENTOS
+            </Titulo>
+            <NovosLivrosContainer>
+                { livros.map( livro => (
+                    <img src={livro.src}/>
+                ) ) }
+            </NovosLivrosContainer>
+            <CardRecomenda
+                titulo="Talvez você se interesse por"
+                subtitulo="Angular 11"
+                descricao="Construindo uma aplicação com a plataforma Google"
+                img={imagemLivro}
+            />
         </UltimosLancamentosContainer>
     )
 }
 
-export default UltimosLancamentos;
+export default UltimosLancamentos
